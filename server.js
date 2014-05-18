aaf8efd650dbb9a46391d3f79e07d2acf500d53b
Edgar <edgarmch@edgarmch.(none)>
2014-05-17 22:26:30 -0500

    Esqueleto

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express'),
     stylus = require('stylus'),
     bodyParser = require('body-parser'),
     favicon = require('serve-favicon'),
+    mongoose = require('mongoose'),
     logger = require('morgan');
 
 
@@ -18,7 +19,7 @@ if ('development' == env) {
   app.set('view engine', 'jade');
   app.use(logger('dev'));
   app.use(bodyParser());
-  app.use(favicon(__dirname + '/public/favicon.ico'));
+  app.use(favicon(__dirname + '/public/favicon.ico'));//revisar favicon v.4 express
   app.use(stylus.middleware({
       src: __dirname + '/public',
       compile: compile
@@ -26,9 +27,29 @@ if ('development' == env) {
   app.use(express.static(__dirname + '/public'));
 }
 
-app.get('*', function(req, res){
-    res.render('index');
+mongoose.connect('mongodb://localhost/mean');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection failed...'));
+db.once('open', function callback(){
+    console.log('mean db opened');
+});
+
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc){
+    mongoMessage = messageDoc.message;
+});
+
+app.get('/partials/:partialPath', function(req, res){//config para angular de jade
+    res.render('partials/' + req.params.partialPath);
 })
+
+app.get('*', function(req, res){//atrapar todas las llamadas a index
+    res.render('index', {
+        mongoMessage: mongoMessage           
+              });
+});
 
 var port= 3030;
 app.listen(port);
